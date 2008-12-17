069aa07ea8dc5f4c82cf8b6e0fe6af9257d6584b
Martin Modahl <mmodahl@gmail.com>
2008-12-16 20:56:25 -0600

    Transliterated Lisp from Ch 1.3

---
+++ lecture2a - ch1.3/Excercises.fs
@@ -0,0 +1,101 @@
+﻿#light
+
+//Exercise 1.29.  Simpson's Rule is a more accurate method of numerical integration than the method illustrated above. Using Simpson's Rule, the integral of a function f between a and b is approximated as
+//
+//where h = (b - a)/n, for some even integer n, and yk = f(a + kh). (Increasing n increases the accuracy of the approximation.) Define a procedure that takes as arguments f, a, b, and n and returns the value of the integral, computed using Simpson's Rule. Use your procedure to integrate cube between 0 and 1 (with n = 100 and n = 1000), and compare the results to those of the integral procedure shown above.
+//
+//Exercise 1.30.  The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. Show how to do this by filling in the missing expressions in the following definition:
+//
+//(define (sum term a next b)
+//  (define (iter a result)
+//    (if <??>
+//        <??>
+//        (iter <??> <??>)))
+//  (iter <??> <??>))
+//
+//Exercise 1.31.  
+//a.  The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures.51 Write an analogous procedure called product that returns the product of the values of a function at points over a given range. Show how to define factorial in terms of product. Also use product to compute approximations to using the formula52
+//
+//b.  If your product procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
+//
+//Exercise 1.32.  a. Show that sum and product (exercise 1.31) are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function:
+//
+//(accumulate combiner null-value term a next b)
+//
+//Accumulate takes as arguments the same term and range specifications as sum and product, together with a combiner procedure (of two arguments) that specifies how the current term is to be combined with the accumulation of the preceding terms and a null-value that specifies what base value to use when the terms run out. Write accumulate and show how sum and product can both be defined as simple calls to accumulate.
+//
+//b. If your accumulate procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
+//
+//Exercise 1.33.  You can obtain an even more general version of accumulate (exercise 1.32) by introducing the notion of a filter on the terms to be combined. That is, combine only those terms derived from values in the range that satisfy a specified condition. The resulting filtered-accumulate abstraction takes the same arguments as accumulate, together with an additional predicate of one argument that specifies the filter. Write filtered-accumulate as a procedure. Show how to express the following using filtered-accumulate:
+//
+//a. the sum of the squares of the prime numbers in the interval a to b (assuming that you have a prime? predicate already written)
+//
+//b. the product of all the positive integers less than n that are relatively prime to n (i.e., all positive integers i < n such that GCD(i,n) = 1). 
+//
+//
+//Exercise 1.34.  Suppose we define the procedure
+//
+//(define (f g)
+//  (g 2))
+//
+//Then we have
+//
+//(f square)
+//4
+//
+//(f (lambda (z) (* z (+ z 1))))
+//6
+//
+//What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain. 
+//
+//Exercise 1.35.  Show that the golden ratio  (section 1.2.2) is a fixed point of the transformation x  1 + 1/x, and use this fact to compute  by means of the fixed-point procedure.
+//
+//Exercise 1.36.  Modify fixed-point so that it prints the sequence of approximations it generates, using the newline and display primitives shown in exercise 1.22. Then find a solution to xx = 1000 by finding a fixed point of x log(1000)/log(x). (Use Scheme's primitive log procedure, which computes natural logarithms.) Compare the number of steps this takes with and without average damping. (Note that you cannot start fixed-point with a guess of 1, as this would cause division by log(1) = 0.)
+//
+//Exercise 1.37.  a. An infinite continued fraction is an expression of the form
+//
+//As an example, one can show that the infinite continued fraction expansion with the Ni and the Di all equal to 1 produces 1/, where is the golden ratio (described in section 1.2.2). One way to approximate an infinite continued fraction is to truncate the expansion after a given number of terms. Such a truncation -- a so-called k-term finite continued fraction -- has the form
+//
+//Suppose that n and d are procedures of one argument (the term index i) that return the Ni and Di of the terms of the continued fraction. Define a procedure cont-frac such that evaluating (cont-frac n d k) computes the value of the k-term finite continued fraction. Check your procedure by approximating 1/ using
+//
+//(cont-frac (lambda (i) 1.0)
+//           (lambda (i) 1.0)
+//           k)
+//
+//for successive values of k. How large must you make k in order to get an approximation that is accurate to 4 decimal places?
+//
+//b. If your cont-frac procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
+//
+//Exercise 1.38.  In 1737, the Swiss mathematician Leonhard Euler published a memoir De Fractionibus Continuis, which included a continued fraction expansion for e - 2, where e is the base of the natural logarithms. In this fraction, the Ni are all 1, and the Di are successively 1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8, .... Write a program that uses your cont-frac procedure from exercise 1.37 to approximate e, based on Euler's expansion.
+//
+//Exercise 1.39.  A continued fraction representation of the tangent function was published in 1770 by the German mathematician J.H. Lambert:
+//
+//where x is in radians. Define a procedure (tan-cf x k) that computes an approximation to the tangent function based on Lambert's formula. K specifies the number of terms to compute, as in exercise 1.37. 
+//
+//Exercise 1.40.  Define a procedure cubic that can be used together with the newtons-method procedure in expressions of the form
+//
+//(newtons-method (cubic a b c) 1)
+//
+//to approximate zeros of the cubic x3 + ax2 + bx + c.
+//
+//Exercise 1.41.  Define a procedure double that takes a procedure of one argument as argument and returns a procedure that applies the original procedure twice. For example, if inc is a procedure that adds 1 to its argument, then (double inc) should be a procedure that adds 2. What value is returned by
+//
+//(((double (double double)) inc) 5)
+//
+//Exercise 1.42.  Let f and g be two one-argument functions. The composition f after g is defined to be the function x f(g(x)). Define a procedure compose that implements composition. For example, if inc is a procedure that adds 1 to its argument,
+//
+//((compose square inc) 6)
+//49
+//
+//Exercise 1.43.  If f is a numerical function and n is a positive integer, then we can form the nth repeated application of f, which is defined to be the function whose value at x is f(f(...(f(x))...)). For example, if f is the function x x + 1, then the nth repeated application of f is the function x x + n. If f is the operation of squaring a number, then the nth repeated application of f is the function that raises its argument to the 2nth power. Write a procedure that takes as inputs a procedure that computes f and a positive integer n and returns the procedure that computes the nth repeated application of f. Your procedure should be able to be used as follows:
+//
+//((repeated square 2) 5)
+//625
+//
+//Hint: You may find it convenient to use compose from exercise 1.42.
+//
+//Exercise 1.44.  The idea of smoothing a function is an important concept in signal processing. If f is a function and dx is some small number, then the smoothed version of f is the function whose value at a point x is the average of f(x - dx), f(x), and f(x + dx). Write a procedure smooth that takes as input a procedure that computes f and returns a procedure that computes the smoothed f. It is sometimes valuable to repeatedly smooth a function (that is, smooth the smoothed function, and so on) to obtained the n-fold smoothed function. Show how to generate the n-fold smoothed function of any given function using smooth and repeated from exercise 1.43.
+//
+//Exercise 1.45.  We saw in section 1.3.3 that attempting to compute square roots by naively finding a fixed point of y x/y does not converge, and that this can be fixed by average damping. The same method works for finding cube roots as fixed points of the average-damped y x/y2. Unfortunately, the process does not work for fourth roots -- a single average damp is not enough to make a fixed-point search for y x/y3 converge. On the other hand, if we average damp twice (i.e., use the average damp of the average damp of y x/y3) the fixed-point search does converge. Do some experiments to determine how many average damps are required to compute nth roots as a fixed-point search based upon repeated average damping of y x/yn-1. Use this to implement a simple procedure for computing nth roots using fixed-point, average-damp, and the repeated procedure of exercise 1.43. Assume that any arithmetic operations you need are available as primitives.
+//
+//Exercise 1.46.  Several of the numerical methods described in this chapter are instances of an extremely general computational strategy known as iterative improvement. Iterative improvement says that, to compute something, we start with an initial guess for the answer, test if the guess is good enough, and otherwise improve the guess and continue the process using the improved guess as the new guess. Write a procedure iterative-improve that takes two procedures as arguments: a method for telling whether a guess is good enough and a method for improving a guess. Iterative-improve should return as its value a procedure that takes a guess as argument and keeps improving the guess until it is good enough. Rewrite the sqrt procedure of section 1.1.7 and the fixed-point procedure of section 1.3.3 in terms of iterative-improve. 
---
+++ lecture2a - ch1.3/Text.fs
@@ -0,0 +1,427 @@
+﻿#light
+
+        
+/// 1.3  Procedures and the Processes They Generate
+//(define (cube x) (* x x x))
+
+let cube x = x * x * x
+
+
+/// 1.3.1  Procedures as Arguments
+
+//(define (sum-integers a b)
+//  (if (> a b)
+//      0
+//      (+ a (sum-integers (+ a 1) b))))
+
+let rec sum_integers a b =
+    if (a > b) then
+        0
+    else
+        (a + (sum_integers (a + 1) b))
+
+
+//(define (sum-cubes a b)
+//  (if (> a b)
+//      0
+//      (+ (cube a) (sum-cubes (+ a 1) b))))
+
+let rec sum_cubes a b =
+    if (a > b) then
+        0
+    else
+        (cube a + (sum_cubes (a + 1) b))
+
+
+//(define (pi-sum a b)
+//  (if (> a b)
+//      0
+//      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+let rec pi_sum (a:float) (b:float) =
+    if (a > b) then
+        0.0
+    else
+         (1.0 / (a * (a + 2.0))) + (pi_sum (a + 4.0) b)
+
+
+//(define (<name> a b)
+//  (if (> a b)
+//      0
+//      (+ (<term> a)
+//         (<name> (<next> a) b))))
+
+//let <name> a b =
+//    if (a > b) then
+//        0
+//    else
+//        (<term> a) + (<name> (<next> a) b)
+
+
+//(define (sum term a next b)
+//  (if (> a b)
+//      0
+//      (+ (term a)
+//         (sum term (next a) next b))))
+
+let rec sum term a next b = 
+    if (a > b) then
+        0
+    else
+        (term a) + (sum term (next a) next b)
+
+
+//(define (inc n) (+ n 1))
+//(define (sum-cubes a b)
+//  (sum cube a inc b))
+
+let inc n = (n + 1)
+let sum_cubes_alt a b = sum cube a inc b
+
+
+//(sum-cubes 1 10)
+//3025
+
+//(define (identity x) x)
+//
+//(define (sum-integers a b)
+//  (sum identity a inc b))
+
+let identity x = x
+let sum_integers_alt a b = sum identity a inc b
+
+//(define (pi-sum a b)
+//  (define (pi-term x)
+//    (/ 1.0 (* x (+ x 2))))
+//  (define (pi-next x)
+//    (+ x 4))
+//  (sum pi-term a pi-next b))
+
+let rec sum_float term a next b = 
+    if (a > b) then
+        0.0
+    else
+        (term a) + (sum_float term (next a) next b)
+
+let pi_sum_alt (a:float) (b:float) = 
+    let pi_term (x:float) = 
+        (x * (x + 2.0)) / 1.0
+    let pi_next (x:float) = x + 4.0
+    (sum_float pi_term a pi_next b)
+    
+    
+//(define (integral f a b dx)
+//  (define (add-dx x) (+ x dx))
+//  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+//     dx))
+
+let integral f a b dx = 
+    let add_dx x = (x + dx)
+    (sum_float f (a + (dx / 2.0)) add_dx b) * dx
+    
+
+//(integral cube 0 1 0.01)
+//.24998750000000042
+//(integral cube 0 1 0.001)
+//.249999875000001
+
+let cube_float x:float = x * x * x
+
+printfn "%f" (integral cube_float 0.0 1.0 0.01)
+//      -> val it : float = 0.2499875
+printfn "%f" (integral cube_float 0.0 1.0 0.001)
+//      -> val it : float = 0.249999875
+//      but prints 0.250000
+
+/// 1.3.2  Constructing Procedures Using Lambda
+
+//(lambda (x) (+ x 4))
+//(fun x -> x + 4)
+
+//(lambda (x) (/ 1.0 (* x (+ x 2))))
+//(fun x ->  1.0 / (x * (x+2.0))
+
+
+
+//(define (pi-sum a b)
+//  (sum (lambda (x) (/ 1.0 (* x (+ x 2))))
+//       a
+//       (lambda (x) (+ x 4))
+//       b))
+
+let pi_sum_lambda a b = 
+    (sum_float (fun x -> 1.0 / (x * (x + 2.0))) a (fun x -> x + 4.0) b)
+    
+
+let sum_integers_lambda a b = 
+    sum (fun x -> x) a (fun x -> x+1) b
+    
+//(define (integral f a b dx)
+//  (* (sum f
+//          (+ a (/ dx 2.0))
+//          (lambda (x) (+ x dx))
+//          b)
+//     dx))
+
+let integral_lambda f a b dx = 
+    (sum_float f (a + (dx / 2.0)) (fun x -> x + dx) b) * dx
+    
+    
+//(lambda (<formal-parameters>) <body>)
+//(fun <formal-parameters> -> <body>)
+
+
+
+/// Using let to create local variables
+//(define (f x y)
+//  (define (f-helper a b)
+//    (+ (* x (square a))
+//       (* y b)
+//       (* a b)))
+//  (f-helper (+ 1 (* x y)) 
+//            (- 1 y)))
+
+let f x y = 
+    let square a = a * a
+    let f_helper a b = 
+       ((square a) * x) + (y * b) + (a * b)
+    (f_helper (1 + (x * y)) (1 - y))
+
+//(define (f x y)
+//  ((lambda (a b)
+//     (+ (* x (square a))
+//        (* y b)
+//        (* a b)))
+//   (+ 1 (* x y))
+//   (- 1 y)))
+
+
+let f_alt x y = 
+    let square a = a * a
+    ((fun a b -> ((square a) * x) + (y * b) + (a * b)) (1 + (x * y)) (1 - y))
+    
+//(define (f x y)
+//  (let ((a (+ 1 (* x y)))
+//        (b (- 1 y)))
+//    (+ (* x (square a))
+//       (* y b)
+//       (* a b))))
+
+let f_let x y =
+    let square j = j * j
+    let a = 1 + x * y
+    let b = 1 - y
+    (x * square a) + (y * b) + (a * b)
+    
+
+
+/// 1.3.3  Procedures as General Methods
+
+
+/// Finding roots of equations by the half-interval method
+
+//(define (search f neg-point pos-point)
+//  (let ((midpoint (average neg-point pos-point)))
+//    (if (close-enough? neg-point pos-point)
+//        midpoint
+//        (let ((test-value (f midpoint)))
+//          (cond ((positive? test-value)
+//                 (search f neg-point midpoint))
+//                ((negative? test-value)
+//                 (search f midpoint pos-point))
+//                (else midpoint))))))
+
+//(define (close-enough? x y)
+//  (< (abs (- x y)) 0.001))
+
+let abs (x:float) = if x < 0.0 then x * -1.0 else x
+let average (x:float) (y:float) = (x + y) / 2.0
+let close_enoughQ (x:float) (y:float) = abs (x-y) < 0.001
+
+let positiveQ (x:float) = x > 0.0
+let negativeQ (x:float) = x < 0.0
+
+let rec search f (neg_point:float) (pos_point:float) = 
+    let midpoint = average neg_point pos_point
+    if close_enoughQ neg_point pos_point then
+        midpoint
+    else
+        let test_value = f midpoint
+        if (positiveQ test_value) then
+            search f neg_point midpoint
+        else 
+            if (negativeQ test_value) then
+                search f midpoint pos_point
+            else
+                midpoint
+        
+//(define (half-interval-method f a b)
+//  (let ((a-value (f a))
+//        (b-value (f b)))
+//    (cond ((and (negative? a-value) (positive? b-value))
+//           (search f a b))
+//          ((and (negative? b-value) (positive? a-value))
+//           (search f b a))
+//          (else
+//           (error "Values are not of opposite sign" a b)))))        
+        
+let half_interval_method f (a:float) (b:float) =
+    let a_value = f a
+    let b_value = f b
+    
+    if (negativeQ a_value) && (positiveQ b_value) then
+        search f a b
+    else if 
+        (negativeQ b_value) && (positiveQ a_value) then
+        search f b a
+    else
+        printfn "Fail Dude\n" |> ignore
+        0.0
+         
+half_interval_method sin 2.0 4.0
+
+//(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+//                      1.0
+//                      2.0)
+
+half_interval_method (fun x -> x * x * x - 2.0 * x - 3.0) 1.0 2.0
+
+
+
+//Finding fixed points of functions
+
+//(define tolerance 0.00001)
+//(define (fixed-point f first-guess)
+//  (define (close-enough? v1 v2)
+//    (< (abs (- v1 v2)) tolerance))
+//  (define (try guess)
+//    (let ((next (f guess)))
+//      (if (close-enough? guess next)
+//          next
+//          (try next))))
+//  (try first-guess))    
+
+let tolerance = 0.000001
+let fixed_point f first_guess = 
+    let close_enoughQ (v1:float) (v2:float) = abs (v1-v2) < tolerance
+    let rec try_ guess = 
+        let next = f guess
+        if close_enoughQ guess next then
+            next
+        else
+            try_ next
+    try_ first_guess
+         
+fixed_point cos 1.0
+
+//(fixed-point (lambda (y) (+ (sin y) (cos y)))
+//             1.0)
+
+fixed_point (fun (y:float) -> sin y + cos y) 1.0
+
+
+//(define (sqrt x)
+//  (fixed-point (lambda (y) (average y (/ x y)))
+//               1.0))
+
+let sqrt x = 
+    let average (f1:float) (f2:float) = (f1 + f2) / 2.0
+    fixed_point (fun y -> printfn "%f/%f = %f" x y (x/y) |> ignore 
+                          (average y (x/y)) ) 1.0
+    
+
+
+/// 1.3.4  Procedures as Returned Values
+
+//(define (average-damp f)
+//  (lambda (x) (average x (f x))))
+
+let average_damp f =
+    let average (i:float) (j:float) = (i + j) / 2.0
+    (fun x -> average x (f x))
+    
+let square x = x * x
+
+(average_damp square) 10
+
+//(define (sqrt x)
+//  (fixed-point (average-damp (lambda (y) (/ x y)))
+//               1.0))
+
+let sqrt x =
+    fixed_point (average_damp (fun y -> x/y)) 1.0
+
+sqrt 9.0
+
+
+//(define (cube-root x)
+//  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+//               1.0))
+
+let cube_root x = 
+    let square i:float = i * i
+    fixed_point (average_damp (fun y -> (x / square y))) 1.0
+
+    
+/// Newton's method
+
+//(define (deriv g)
+//  (lambda (x)
+//    (/ (- (g (+ x dx)) (g x))
+//       dx)))
+//(define dx 0.00001)
+
+let dx = 0.00001
+let deriv g = 
+    (fun x -> (g(x + dx) - (g x)) / dx)
+
+let cube (f:float) = f * f * f
+cube 5.0
+(deriv cube) 5.0
+    
+    
+//(define (newton-transform g)
+//  (lambda (x)
+//    (- x (/ (g x) ((deriv g) x)))))
+//(define (newtons-method g guess)
+//  (fixed-point (newton-transform g) guess))
+
+let newton_transform g = 
+    (fun x -> (x - (g x) / ((deriv g) x)))
+let newton_method g guess = 
+    fixed_point (newton_transform g) guess
+    
+//(define (sqrt x)
+//  (newtons-method (lambda (y) (- (square y) x))
+//                  1.0))
+
+let sqrt x = 
+    let square (f:float) = f*f
+    newton_method (fun y -> square y - x) 1.0
+
+
+
+/// Abstractions and first-class procedures
+
+//(define (fixed-point-of-transform g transform guess)
+//  (fixed-point (transform g) guess))
+
+let fixed_point_of_transform g transform guess = 
+    (fixed_point (transform g) guess)
+
+//(define (sqrt x)
+//  (fixed-point-of-transform (lambda (y) (/ x y))
+//                            average-damp
+//                            1.0))
+
+let sqrt x = fixed_point_of_transform (fun y -> x/y) average_damp 1.0
+
+
+//(define (sqrt x)
+//  (fixed-point-of-transform (lambda (y) (- (square y) x))
+//                            newton-transform
+//                            1.0))
+
+let sqrt x = 
+    let square (f:float) = f * f
+    fixed_point_of_transform (fun y -> square y - x) newton_transform 1.0
