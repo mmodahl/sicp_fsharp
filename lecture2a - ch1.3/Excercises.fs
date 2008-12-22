e187bbe2290a5e761d4d51d76ea0712152efe93b
Martin Modahl <mmodahl@gmail.com>
2008-12-22 08:53:24 -0600

    Lecture 2a watched...

--- lecture2a - ch1.3/Excercises.fs
+++ lecture2a - ch1.3/Excercises.fs
@@ -1,10 +1,20 @@
 ﻿#light
 
-//Exercise 1.29.  Simpson's Rule is a more accurate method of numerical integration than the method illustrated above. Using Simpson's Rule, the integral of a function f between a and b is approximated as
+//Exercise 1.29.  Simpson's Rule is a more accurate method of numerical integration than 
+//the method illustrated above. Using Simpson's Rule, the integral of a function f between 
+//a and b is approximated as
 //
-//where h = (b - a)/n, for some even integer n, and yk = f(a + kh). (Increasing n increases the accuracy of the approximation.) Define a procedure that takes as arguments f, a, b, and n and returns the value of the integral, computed using Simpson's Rule. Use your procedure to integrate cube between 0 and 1 (with n = 100 and n = 1000), and compare the results to those of the integral procedure shown above.
-//
-//Exercise 1.30.  The sum procedure above generates a linear recursion. The procedure can be rewritten so that the sum is performed iteratively. Show how to do this by filling in the missing expressions in the following definition:
+//where h = (b - a)/n, for some even integer n, and yk = f(a + kh). (Increasing n increases 
+//the accuracy of the approximation.) Define a procedure that takes as arguments f, a, b, 
+//and n and returns the value of the integral, computed using Simpson's Rule. Use your 
+//procedure to integrate cube between 0 and 1 (with n = 100 and n = 1000), and compare the 
+//results to those of the integral procedure shown above.
+
+
+
+//Exercise 1.30.  The sum procedure above generates a linear recursion. The procedure can 
+//be rewritten so that the sum is performed iteratively. Show how to do this by filling in 
+//the missing expressions in the following definition:
 //
 //(define (sum term a next b)
 //  (define (iter a result)
@@ -12,27 +22,72 @@
 //        <??>
 //        (iter <??> <??>)))
 //  (iter <??> <??>))
-//
+
+//(define (sum term a next b)
+//  (define (iter a result)
+//    (if a > b
+//        result
+//        (iter (next a) (+ result (term a)))))
+//  (iter a 0))
+
+let sum term a next b = 
+    let rec iter a result = 
+        if (a > b) then
+            result
+        else
+            (iter (next a) (result + (term a)))
+    iter a 0
+
+
 //Exercise 1.31.  
-//a.  The sum procedure is only the simplest of a vast number of similar abstractions that can be captured as higher-order procedures.51 Write an analogous procedure called product that returns the product of the values of a function at points over a given range. Show how to define factorial in terms of product. Also use product to compute approximations to using the formula52
-//
-//b.  If your product procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
-//
-//Exercise 1.32.  a. Show that sum and product (exercise 1.31) are both special cases of a still more general notion called accumulate that combines a collection of terms, using some general accumulation function:
+//a.  The sum procedure is only the simplest of a vast number of similar abstractions that 
+//can be captured as higher-order procedures.51 Write an analogous procedure called product 
+//that returns the product of the values of a function at points over a given range. 
+//Show how to define factorial in terms of product. Also use product to compute 
+//approximations to using the formula52
+
+//b.  If your product procedure generates a recursive process, write one that generates an 
+//iterative process. If it generates an iterative process, write one that generates a 
+//recursive process.
+
+
+
+//Exercise 1.32.  a. Show that sum and product (exercise 1.31) are both special cases of a 
+//still more general notion called accumulate that combines a collection of terms, using 
+//some general accumulation function:
 //
 //(accumulate combiner null-value term a next b)
 //
-//Accumulate takes as arguments the same term and range specifications as sum and product, together with a combiner procedure (of two arguments) that specifies how the current term is to be combined with the accumulation of the preceding terms and a null-value that specifies what base value to use when the terms run out. Write accumulate and show how sum and product can both be defined as simple calls to accumulate.
-//
-//b. If your accumulate procedure generates a recursive process, write one that generates an iterative process. If it generates an iterative process, write one that generates a recursive process.
-//
-//Exercise 1.33.  You can obtain an even more general version of accumulate (exercise 1.32) by introducing the notion of a filter on the terms to be combined. That is, combine only those terms derived from values in the range that satisfy a specified condition. The resulting filtered-accumulate abstraction takes the same arguments as accumulate, together with an additional predicate of one argument that specifies the filter. Write filtered-accumulate as a procedure. Show how to express the following using filtered-accumulate:
-//
-//a. the sum of the squares of the prime numbers in the interval a to b (assuming that you have a prime? predicate already written)
-//
-//b. the product of all the positive integers less than n that are relatively prime to n (i.e., all positive integers i < n such that GCD(i,n) = 1). 
-//
-//
+//Accumulate takes as arguments the same term and range specifications as sum and product, 
+//together with a combiner procedure (of two arguments) that specifies how the current term 
+//is to be combined with the accumulation of the preceding terms and a null-value that 
+//specifies what base value to use when the terms run out. Write accumulate and show how 
+//sum and product can both be defined as simple calls to accumulate.
+
+//b. If your accumulate procedure generates a recursive process, write one that generates 
+//an iterative process. If it generates an iterative process, write one that generates a 
+//recursive process.
+
+
+
+
+//Exercise 1.33.  You can obtain an even more general version of accumulate (exercise 1.32) 
+//by introducing the notion of a filter on the terms to be combined. That is, combine only 
+//those terms derived from values in the range that satisfy a specified condition. The 
+//resulting filtered-accumulate abstraction takes the same arguments as accumulate, 
+//together with an additional predicate of one argument that specifies the filter. Write 
+//filtered-accumulate as a procedure. Show how to express the following using 
+//filtered-accumulate:
+
+//a. the sum of the squares of the prime numbers in the interval a to b (assuming that you 
+//have a prime? predicate already written)
+
+
+//b. the product of all the positive integers less than n that are relatively prime to n 
+//(i.e., all positive integers i < n such that GCD(i,n) = 1). 
+
+
+
 //Exercise 1.34.  Suppose we define the procedure
 //
 //(define (f g)
@@ -46,15 +101,33 @@
 //(f (lambda (z) (* z (+ z 1))))
 //6
 //
-//What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? Explain. 
-//
-//Exercise 1.35.  Show that the golden ratio  (section 1.2.2) is a fixed point of the transformation x  1 + 1/x, and use this fact to compute  by means of the fixed-point procedure.
-//
-//Exercise 1.36.  Modify fixed-point so that it prints the sequence of approximations it generates, using the newline and display primitives shown in exercise 1.22. Then find a solution to xx = 1000 by finding a fixed point of x log(1000)/log(x). (Use Scheme's primitive log procedure, which computes natural logarithms.) Compare the number of steps this takes with and without average damping. (Note that you cannot start fixed-point with a guess of 1, as this would cause division by log(1) = 0.)
-//
+//What happens if we (perversely) ask the interpreter to evaluate the combination (f f)? 
+//Explain. 
+
+
+
+//Exercise 1.35.  Show that the golden ratio  (section 1.2.2) is a fixed point of the 
+//transformation x  1 + 1/x, and use this fact to compute  by means of the fixed-point 
+//procedure.
+
+
+
+//Exercise 1.36.  Modify fixed-point so that it prints the sequence of approximations it 
+//generates, using the newline and display primitives shown in exercise 1.22. Then find a 
+//solution to xx = 1000 by finding a fixed point of x log(1000)/log(x). 
+//(Use Scheme's primitive log procedure, which computes natural logarithms.) Compare the 
+//number of steps this takes with and without average damping. (Note that you cannot start 
+//fixed-point with a guess of 1, as this would cause division by log(1) = 0.)
+
+
+
 //Exercise 1.37.  a. An infinite continued fraction is an expression of the form
 //
-//As an example, one can show that the infinite continued fraction expansion with the Ni and the Di all equal to 1 produces 1/, where is the golden ratio (described in section 1.2.2). One way to approximate an infinite continued fraction is to truncate the expansion after a given number of terms. Such a truncation -- a so-called k-term finite continued fraction -- has the form
+//As an example, one can show that the infinite continued fraction expansion with the 
+//Ni and the Di all equal to 1 produces 1/, where is the golden ratio (described in 
+//section 1.2.2). One way to approximate an infinite continued fraction is to truncate 
+//the expansion after a given number of terms. Such a truncation -- a so-called k-term 
+//finite continued fraction -- has the form
 //
 //Suppose that n and d are procedures of one argument (the term index i) that return the Ni and Di of the terms of the continued fraction. Define a procedure cont-frac such that evaluating (cont-frac n d k) computes the value of the k-term finite continued fraction. Check your procedure by approximating 1/ using
 //
